c13e0164eb539c2ad8fe1fc3a5b474ef2e550a59
Astrid Leon <astrid@debian-BULLSEYE-live-builder-AMD64>
2024-06-23 13:50:43 -0400

    simulador nuevo, uso de funciones basicas

--- simulador.py
+++ simulador.py
@@ -2,30 +2,48 @@ from persona import Persona
 from comunidad import Comunidad
 from enfermedad import Enfermedad
 import random
+import pandas as pd
 
-# prueba de simulador para interactuar con las clases
 
-covid = Enfermedad('covid',20, random.randint(1,40))
-talca = Comunidad()
-talca.add_enfermedad(covid)
+    # prueba de simulador para interactuar con las clases
+    # draft crea una comunidad para controlar el manejo del contagio
+    # y como ocurren (aun no se usan formulas) (se usa pandas)
 
-persona1 = Persona(1,'Paco','Pipipi')
-persona2 = Persona(2,'Peca','Pipipi')
+def crea_comunidad():
+    datos = pd.read_csv('nombres.csv')
+    nombres = pd.DataFrame(datos)
+    comunidad = Comunidad()
+    miembros_comunidad = random.randint(100,8000)
+    ident = 0
 
-talca.agregar_persona_comunidad(persona1)
-talca.agregar_persona_comunidad(persona2)
+    for i in range(miembros_comunidad):
+        # a corresponde al nombre aleatorio del archivo
+        # y p al apellido
+        a = random.randint(0,299)
+        p = random.randint(0,299)
+        # draft infectados iniciales
+        infectado = random.randint(0,100)
+        ident += 1
+        nombre_ciudadano = nombres.iloc[a,0]
+        apellido_ciudadano = nombres.iloc[p,1]
+        ciudadano = Persona(ident,nombre_ciudadano,apellido_ciudadano)
+        # probabilidad del 1% de que una persona sea contagio inicial
+        if infectado == 1:
+            ciudadano.set_contagiado(True)
+        else:
+            ciudadano.set_contagiado(False)
+        #se agrega a la lista de ciudadanos en la comunidad
+        comunidad.agregar_persona_comunidad(ciudadano)
+    contar_contagiados_comunidad(comunidad)
+            
 
-# draft funcionamiento del contagio
+def contar_contagiados_comunidad(comunidad):
+    contador_contagiados = 0
+    lista_ciudadanos_comunidad = comunidad.retorno_lista_comunidad()
+    for i in lista_ciudadanos_comunidad:
+        if i.get_contagiado():
+            contador_contagiados += 1
 
-persona2.set_contagiado(True)
+    print(contador_contagiados, "contagios iniciales de", len(lista_ciudadanos_comunidad))
 
-#valor es un bool
-
-valor = talca.contacto_estrecho(persona1,persona2,covid)
-persona1.set_contagiado(valor)
-
-if persona1.get_contagiado() == True:
-    print(f"{persona1.get_nombre()} se ha enfermado")
-else:
-    print("nope")
-
+crea_comunidad()
